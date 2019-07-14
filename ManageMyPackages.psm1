d2241a4c00b95459de561eb5e10d088ac2402bc6
shskwmt <shskwmt@outlook.jp>
2019-07-15 03:00:36 +0900

    Fix indentation

--- ManageMyPackages.psm1
+++ ManageMyPackages.psm1
@@ -5,52 +5,53 @@
 Set-Variable -Name WindowsPackageListFile -Value "WindowsPackageList.txt" -Option Constant
 
 Function Install-PackageProviders {
-  <#
-  .SYNOPSIS
+    <#
+    .SYNOPSIS
     Install package providers
-  #>
-  Param (
-    [Parameter(Position=0)][String[]]$ProviderList = @(
-      "NuGet",
-      "ChocolateyGet",
-      "PowerShellGet",
-      "Chocolatey"
+    #>
+    Param (
+         [Parameter(Position=0)]
+         [String[]]$ProviderList = @(
+             "NuGet",
+             "ChocolateyGet",
+             "PowerShellGet",
+             "Chocolatey"
+         )
     )
-  )
-  $ProviderList | Get-Unique | ForEach-Object {
-    Write-Host "Install $_ ..."
-    Install-PackageProvider -Force $_
-  }
+    $ProviderList | Get-Unique | ForEach-Object {
+        Write-Host "Install $_ ..."
+        Install-PackageProvider -Force $_
+    }
 }
 
 Function Install-MyPackages {
-  <#
-  .SYNOPSIS
+    <#
+    .SYNOPSIS
     Install packages that are listed in gist.
-  #>
-  Param(
-    [Parameter(Mandatory,Position=0)][String]$PackageListGistId,
-    [Parameter(Position=1)][String]$FileName = $WindowsPackageListFile,
-    [Parameter(Position=2)]
-    [ValidateSet("NuGet","ChocolateyGet","PowerShellGet","Chocolatey")]
-    [String]$ProviderName = "ChocolateyGet"
-  )
-  [System.Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12 -bor [Net.SecurityProtocolType]::Tls11
-  $gistUri = "https://api.github.com/gists/" + $PackageListGistId
-  $packageList = (Invoke-WebRequest -Uri $gistUri | ConvertFrom-Json).files.$fileName.content -split "\n"
-  $packageList | ForEach-Object {
-    Write-Host "Install $_ ..."
-    Install-Package -Force -Provider $ProviderName $_}
-}
+    #>
+    Param(
+        [Parameter(Mandatory,Position=0)][String]$PackageListGistId,
+        [Parameter(Position=1)][String]$FileName = $WindowsPackageListFile,
+        [Parameter(Position=2)]
+        [ValidateSet("NuGet","ChocolateyGet","PowerShellGet","Chocolatey")]
+        [String]$ProviderName = "ChocolateyGet"
+    )
+    [System.Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12 -bor [Net.SecurityProtocolType]::Tls11
+    $gistUri = "https://api.github.com/gists/" + $PackageListGistId
+    $packageList = (Invoke-WebRequest -Uri $gistUri | ConvertFrom-Json).files.$fileName.content -split "\n"
+    $packageList | ForEach-Object {
+        Write-Host "Install $_ ..."
+        Install-Package -Force -Provider $ProviderName $_}
+    }
 
-Function Out-PackageListFile {
-  <#
-  .SYNOPSIS
-    Output package list file that you have already installed.
-  #>
-  Param(
-    [Parameter(Position=0)][String]$PackageListPath = $WindowsPackageListFile,
-    [Parameter(Position=1)][String]$Provider = "ChocolateyGet"
-  )
-  Get-Package -Provider $Provider | ForEach-Object {$_.Name} | Out-File $PackageListPath
-}
+    Function Out-PackageListFile {
+        <#
+        .SYNOPSIS
+        Output package list file that you have already installed.
+        #>
+        Param(
+            [Parameter(Position=0)][String]$PackageListPath = $WindowsPackageListFile,
+            [Parameter(Position=1)][String]$Provider = "ChocolateyGet"
+        )
+        Get-Package -Provider $Provider | ForEach-Object {$_.Name} | Out-File $PackageListPath
+    }
